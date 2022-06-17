92787ddb2fe186072fcb69b5cfa7e598cf5b9c51
Daniel <daniel@testpress.in>
2022-06-17 13:41:56 +0530

    Add get_absolure_url method to Category & Products

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -17,14 +18,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
 
 class Product(models.Model):
-    catagory = models.ForeignKey(Category,
+    category = models.ForeignKey(Category,
                                  related_name='products',
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='media/products/%y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -37,3 +42,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
