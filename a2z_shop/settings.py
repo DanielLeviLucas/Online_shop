23c293bc8806758bb9d64dda8c59e3a8caf803f1
Daniel <daniellevilucas@gmail.com>
2022-06-20 02:59:53 +0530

    Generate PDF invoices dynamically

--- a2z_shop/settings.py
+++ a2z_shop/settings.py
@@ -131,6 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # django sessions
 
