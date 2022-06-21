ef4201666da4581b36494a7df45f255dbcf39f89
Daniel <daniel@testpress.in>
2022-06-21 12:41:45 +0530

    Add redis to dependency package

--- a2z_shop/settings.py
+++ a2z_shop/settings.py
@@ -134,6 +134,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+# Redis
+
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = 6379
+REDIS_DB = 1
+
 # django sessions
 
 CART_SESSION_ID = 'cart'
