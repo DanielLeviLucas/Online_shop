2a183d67c67e6a3238356821833ffa09137e76c8
Daniel <daniel@testpress.in>
2022-06-21 12:29:05 +0530

    Fix assignment operator

--- orders/views.py
+++ orders/views.py
@@ -40,8 +40,9 @@ def order_create(request):
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save(commit=False)
+            print(cart.coupon)
             if cart.coupon:
-                order.coupon - cart.coupon
+                order.coupon = cart.coupon
                 order.discount = cart.coupon.discount
             order.save()
             for item in cart:
